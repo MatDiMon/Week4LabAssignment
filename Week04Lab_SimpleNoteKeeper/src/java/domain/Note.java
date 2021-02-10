17e3a8b1fde2423360fc501be73cfc2b1a79b98b
830391 <830391@SAIT222421IT.ACDM.DS.SAIT.CA>
2021-02-09 23:28:16 -0700

    Week 3 Lab assignment.

---
+++ Week04Lab_SimpleNoteKeeper/src/java/domain/Note.java
@@ -0,0 +1,31 @@
+
+ 
+package domain;
+
+public class Note {
+
+    private String title;
+    private String contents;
+
+    public Note() {
+        this.title = "";
+        this.contents = "";
+    }
+    public Note(String title, String contents) {
+        this.title = title;
+        this.contents = contents;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getContents() {
+        return contents;
+    }
+    public void setContents(String content) {
+        this.contents = content;
+    }
+
+}
